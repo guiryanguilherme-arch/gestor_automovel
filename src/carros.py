dc7bf1c6b705304c2745b537d7d242305b5a6015
Ryan Brito <guiryanguilherme@gmail.com>
2026-04-29 22:21:54 +0100

    ass commit id cliente in file "carro" in def adicionar_carro

--- src/carros.py
+++ src/carros.py
@@ -43,6 +43,7 @@ def adicionar_carro(marca, modelo, matricula, ano, mes, combustivel, potencia_cv
 
     carro = {
         "id":            gerar_id(carros),    # ID único gerado automaticamente
+        "id_cliente": id_cliente, 
         "marca":         marca,
         "modelo":        modelo,
         "matricula":     matricula.upper(),   # Guardada sempre em maiúsculas
